7d8162bf8a0667984525289fb59b61d5b8e360e1
Parmanand Parihar <raj21parihar@gmail.com>
2023-08-16 23:01:34 +0530

    added 2665

--- Practice/2665-counter-II.js
+++ Practice/2665-counter-II.js
@@ -1,10 +1,14 @@
-// 2667. Create Hello World Function
+// 2665. Counter II
 
 /* 
-Write a function create HelloWorld. It should return a new function that always returns "Hello World".
+Write a function createCounter. It should accept an initial integer init. It should return an object with three functions.
+The three functions are:
+    increment() increases the current value by 1 and then returns it.
+    decrement() reduces the current value by 1 and then returns it.
+    reset() sets the current value to init and then returns it.
 */
 
-// Solution
+//Solution
 
 /**
  * @param {integer} init
--- Practice/2704-to-be-or-not-to-be.js
+++ Practice/2704-to-be-or-not-to-be.js
@@ -1,6 +1,7 @@
 // 2704. To Be Or Not To Be
 
-/* Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
+/*
+Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
     1. toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
     2. notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal".
 */

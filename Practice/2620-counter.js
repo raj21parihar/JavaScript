1f4e98e37df418a77be10e32013c6cfccdc46c8e
Parmanand Parihar <raj21parihar@gmail.com>
2023-08-16 22:56:51 +0530

    added 2620

---
+++ Practice/2620-counter.js
@@ -0,0 +1,25 @@
+// 2620. Counter
+
+/* Given an integer n, return a counter function. This counter function initially returns n and then returns 1
+more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+ */
+
+//Solution
+
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function (n) {
+    let num = n;
+    return function () {
+        return num++;
+    };
+};
+
+/**
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */

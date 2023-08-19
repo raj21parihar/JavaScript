f9fc4b5e2ea1becde63c8c42aa6baa972aac5834
Parmanand Parihar <raj21parihar@gmail.com>
2023-08-19 14:29:57 +0530

    updated 2723

--- Practice/2723-add-two-promises.js
+++ Practice/2723-add-two-promises.js
@@ -5,4 +5,19 @@ Given two promises promise1 and promise2, return a new promise. promise1 and pro
 The returned promise should resolve with the sum of the two numbers.
  */
 
-//
+//Solution
+
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function (promise1, promise2) {
+    const [val1, val2] = await Promise.all([promise1, promise2]);
+    return val1 + val2;
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */

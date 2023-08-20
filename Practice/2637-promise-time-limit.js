27e6bfb834b544b8b343d11b2b6bad24d2d8b0d7
Parmanand Parihar <raj21parihar@gmail.com>
2023-08-20 19:28:23 +0530

    added 2637.

--- Practice/2637-promise-time-limit.js
+++ Practice/2637-promise-time-limit.js
@@ -6,7 +6,7 @@ fn takes arguments provided to the time limited function.
 The time limited function should follow these rules:
     If the fn completes within the time limit of t milliseconds, the time limited function should resolve with the result.
     If the execution of the fn exceeds the time limit, the time limited function should reject with the string "Time Limit Exceeded".
-  */
+*/
 
 //Solution
 

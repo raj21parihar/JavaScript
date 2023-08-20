d1950f6bc1093887b6e241979e0e14e29443edb4
Parmanand Parihar <raj21parihar@gmail.com>
2023-08-20 19:38:16 +0530

    added 2725

---
+++ Practice/2725-interval-cancellation.js
@@ -0,0 +1,54 @@
+// 2725. Interval Cancellation
+
+/* 
+Given a function fn, an array of arguments args, and an interval time t, return a cancel function cancelFn.
+The function fn should be called with args immediately and then called again every t milliseconds until cancelFn is called at cancelT ms.
+ */
+
+//Solution
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function (fn, args, t) {
+    fn(...args);
+    const cancelControl = setInterval(function () {
+        fn(...args);
+    }, t);
+    return function () {
+        clearInterval(cancelControl);
+    };
+};
+
+/**
+ *  const result = []
+ *
+ *  const fn = (x) => x * 2
+ *  const args = [4], t = 20, cancelT = 110
+ *
+ *  const start = performance.now()
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start)
+ *      result.push({"time": diff, "returned": fn(...argsArr)})
+ *  }
+ *
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  setTimeout(() => {
+ *     cancel()
+ *  }, cancelT)
+ *
+ *  setTimeout(() => {
+ *    console.log(result)  // [
+ *                         //      {"time":0,"returned":8},
+ *                         //      {"time":20,"returned":8},
+ *                         //      {"time":40,"returned":8},
+ *                         //      {"time":60,"returned":8},
+ *                         //      {"time":80,"returned":8},
+ *                         //      {"time":100,"returned":8}
+ *                         //  ]
+ *  }, cancelT + t + 15)
+ */
